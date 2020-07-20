1fb7c2afd06c8675090aa325a525d0674d3309f8
Uzbekmind <67470704+Uzbekmind@users.noreply.github.com>
2020-07-21 02:40:09 +0500

    Update antiphishuz.py

--- antiphishuz.py
+++ antiphishuz.py
@@ -1,10 +1,3 @@
-FG = 'Fore.GREEN'
-FC = 'Fore.CYAN'
-FW = 'Fore.WHITE'
-FY = 'Fore.YELLOW'
-FM = 'Fore.MAGENTA'
-FB = 'Fore.BLUE'
-FR = 'Fore.RED'
 
 
 import requests
@@ -17,7 +10,7 @@ def cls():
     os.system('clear')
 def banner():
 
-    print(FG + '''
+    print(Fore.GREEN + '''
        ___          _    _ ______  _       _       __   __ ________
       / _ \        | |  (_)|  ___|(_)     | |     | |  | | \_____  \
      / /_\ \ _ __  | |_  _ | |_    _  ___ | |__   | |  | |       / /
@@ -35,17 +28,17 @@ def banner():
      \_____/(_) \___/ 
                   
     ''')
-    print(FC + 'Created by THE UZBEKMIND & Uzbekmind_Public (telegram) & Umchats (telegram Chat')
+    print(Fore.CYAN + 'Created by THE UZBEKMIND & Uzbekmind_Public (telegram) & Umchats (telegram Chat')
     print('--------------------------------------------------------')
     print('\n')             
 cls()
 
 banner()
-print(FM + ' Misol uchun: ' + FW + 'http://saytnomi.com/\n\n')
+print(Fore.MAGENTA + ' Misol uchun: ' + Fore.WHITE + 'http://saytnomi.com/\n\n')
                                 
 file = open('files.txt', 'r')
 
-a = input(FY + "Saytni kiriting: " + FW)
+a = input(Fore.YELLOW + "Saytni kiriting: " + Fore.WHITE)
           
 if a[:-1] != '/':
     a = (a + '/')
@@ -57,60 +50,60 @@ for line in file:
     print(line[:-1])
                  
     try:
-        urllib.request.urlretrieve(a + line[:-1] + '.txt', 'core/' + 'cracked.txt')
+        urllib.request.urlretrieve(a + line[:-1] + '.txt',  + 'cracked.txt')
 
         ok = '1'
     except:
-        print(FR + ' Hujjat ' + str(line) + ' topilmadi!')
+        print(Fore.RED + ' Hujjat ' + str(line) + ' topilmadi!')
                                               
     try:
 
-        urllib.request.urlretrieve(a + line[:-1] + '.log', 'core/' + 'cracked.txt')
+        urllib.request.urlretrieve(a + line[:-1] + '.log',  + 'cracked.txt')
         ok = '1'
     except:                                
-        print(FR + 'Log ' + str(line) + ' topilmadi!')
+        print(Fore.RED + 'Log ' + str(line) + ' topilmadi!')
 
     try:
-        urllib.request.urlretrieve(a + line[:-1]+ '.lst', 'core/' + 'cracked.txt')
+        urllib.request.urlretrieve(a + line[:-1]+ '.lst',  + 'cracked.txt')
         ok = '1'
     except:
-        print(FR + ' Ro`yhat ' + str(line) + ' topilmadi!')
+        print(Fore.RED + ' Ro`yhat ' + str(line) + ' topilmadi!')
     try:
-        urllib.request.urlretrieve(a + line[:-1] + '.db', 'core/' + 'cracked.db')
+        urllib.request.urlretrieve(a + line[:-1] + '.db',  + 'cracked.db')
         ok = '2'
     except:
-        print(FR + '[!] Ma`lumotlar bazasi ' + str(line) + ' topilmadi!')
+        print(Fore.RED + '[!] Ma`lumotlar bazasi ' + str(line) + ' topilmadi!')
         if int(ok) >= 1:
-            print(FG + '[!] Tabriklaymiz! Siz fishing saytni egallab oldingiz!!!! Fayl: ' + line)
+            print(Fore.GREEN + '[!] Tabriklaymiz! Siz fishing saytni egallab oldingiz!!!! Fayl: ' + line)
             print('')
-            print(FM + '[!]Fayldagi malumotlar yuklanmoqda...' + FW + '\n\n')
+            print(Fore.MAGENTA + '[!]Fayldagi malumotlar yuklanmoqda...' + Fore.WHITE + '\n\n')
             os.system('cat core/cracked.txt')
-            print(FM + '[!] 15 sekund davomida yuqoridagi natijani korishingiz mumkin, undan keyin davom etamiz!')
+            print(Fore.MAGENTA + '[!] 15 sekund davomida yuqoridagi natijani korishingiz mumkin, undan keyin davom etamiz!')
             time.sleep(15)
             ok = 0
             if ok == '1':
 
-                print(FC + '[!] core/cracked.txt nomli faylga saqlandi!')
+                print(Fore.CYAN + '[!] cracked.txt nomli faylga saqlandi!')
             elif ok == '2':
-                print(FC + '[!] core/cracked.db nomli faylga saqlandi!')
+                print(Fore.CYAN + '[!] cracked.db nomli faylga saqlandi!')
             print('\n')
-            print(FB + '[!]Qidiruvimiz davom etamoqda... \n')
+            print(Fore.BLUE + '[!]Qidiruvimiz davom etamoqda... \n')
             time.sleep(2)
             a = '1'
 if a == '1':
     cls()
     banner()
 
-    print(FG + '[!]Fayldagi ma`lumotlar yuklanmoqda: \n\n' + FW)
-    print(FY + '----------------------------------------------------------------\n\n')
-    print(FW + '')
-    os.system('cat core/cracked.txt')
-    print(FY + '\n\n----------------------------------------------------------------')
-    q = input(FG + '\nHimoyalangan ma`lumot ^^^')
+    print(Fore.GREEN + '[!]Fayldagi ma`lumotlar yuklanmoqda: \n\n' + Fore.WHITE)
+    print(Fore.YELLOW + '----------------------------------------------------------------\n\n')
+    print(Fore.WHITE + '')
+    os.system('cat cracked.txt')
+    print(Fore.YELLOW + '\n\n----------------------------------------------------------------')
+    q = input(Fore.GREEN + '\nHimoyalangan ma`lumot ^^^')
  
 elif a == '2':
     cls()
     banner()
-    print(FG + '[!]"core/cracked.db" nomli faylga saqlandi!')
+    print(Fore.GREEN + '[!]"cracked.db" nomli faylga saqlandi!')
 else:
-    print(FB + '[!]Voy! Hech nima topilmadi!' + FW)
+    print(Fore.BLUE + '[!]Voy! Hech nima topilmadi!' + Fore.WHITE)
